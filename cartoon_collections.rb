350b17292ae12b95b10fb10cd73b01e99bdc2492
Daniel Hahm <dhahm01@gmail.com>
2020-06-10 14:53:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.map do |element|
     element.count > 4 ? true : false
+  end
 end
 
 def find_valid_calls(planeteer_calls)
